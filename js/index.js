3badfe68e6c4ddc176a6d3b4742577edfdd184c9
Faisha <faishasj@gmail.com>
2021-08-06 16:46:42 +1000

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,35 @@
+/*
+Code adapted from: https://css-tricks.com/snippets/css/typewriter-effect/
+*/
+
+const typeSpeed = 100;
+
+function setupTypewriter(t, HTML) {
+  t.innerHTML = "";
+
+  var cursorPosition = 0,
+      tempTypeSpeed = 0;
+
+  var type = function() {
+
+    if (HTML[cursorPosition] === " ") {
+      tempTypeSpeed = 0;
+    }
+    else {
+      tempTypeSpeed = (Math.random() * typeSpeed) + 50;
+    }
+    
+    t.innerHTML += HTML[cursorPosition];
+
+    hljs.highlightElement(t);
+
+    cursorPosition += 1;
+    if (cursorPosition < HTML.length - 1) {
+      setTimeout(type, tempTypeSpeed);
+    }
+  }
+
+  return {
+    type: type
+  }
+}
---
+++ js/poems.js
@@ -0,0 +1,34 @@
+const poems = [
+`const ant = "rumination";
+var i,ed,
+    fervour;
+
+while (i) {
+  let you : string[] = ["me along"];
+
+  are_we : undefined ?
+
+  /* what do i mean */ 2 : you;
+
+  // all i know is
+  you.reduce((me, to) => null);
+
+  let please = /* make me a */ new Promise(to => {
+      setTimeout(ondrop
+                   /* -ping us */) 
+
+      while (i.filter(the,
+        (red, flags,
+          our,
+        heisenbugs) => {
+         let s, keep, running;
+    }));
+  });
+
+  // i know you need to 
+  // compile your thoughts but
+  please.then(() => {
+    return 2 /* me */;
+  });
+}`
+]
